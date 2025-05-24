d04d9209461013e284c28903d1759f24bd8ebad3
Dr-Lepic <mbmahbub007@gmail.com>
2025-05-25 04:14:49 +0600

    added profileRouter

--- backend/api/routes/profileRouter.js
+++ backend/api/routes/profileRouter.js
@@ -8,18 +8,32 @@ profileRouter.get("/:userID", authUserToken, async (req, res) => {
   try {
     const user = await User.findById(req.params.userID);
     if (!user) throw new Error("Invalid user ID");
+    let profileData = {};
+    if(user.share === false){
+       profileData = {
+          userID: user._id,
+          name: user.name,
+          email: user.email,
+          dob: user.dob,
+        };
+    }else{
+        profileData = {
+          userID: user._id,
+          name: user.name,
+          email: user.email,
+          university: user.university,
+          dept: user.dept,
+          program: user.program,
+          year: user.year,
+          phone: user.phone,
+          dob: user.dob,
+        };
+    }
 
-    // const profileData = {
-    //   userID: user._id,
-    //   name: user.name,
-    //   age: user.age,
-    //   gender: user.gender,
-    //   bio: user.bio,
-    //   photoURL: user.photoURL,
-    //   createdAt: user.createdAt,
-    //   updatedAt: user.updatedAt,
-    // };
-    res.status(200).json({ profile: user });
+    res.status(200).json({
+      success: true,
+      message: "Profile get successful!",
+      data: { user: profileData }, });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
