77d5c4993bdadfa7671788053fc43f89bf751a9c
Raiyan <raiyanmuhtasim@gmail.com>
2025-05-24 09:40:38 +0600

    code cleanup

---
+++ backend/api/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const { sql } = require('@vercel/postgres');
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.status(200).json({"message": "OK"});
+});
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
